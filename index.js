a9271e3b7c3683a84b7eda923c823adc0ed16503
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-12 13:15:56 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const namesArray=["Paul", "Justin", "Adam"];
+
+function writeCards() {
+    let thankYouCards=[];
+    for ( let i=0; i<namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful gift!` );
+    } return thankYouCards;
+  }
+
+console.log(writeCards())
+//
+
+function countdown(startingNum) {
+  let i=startingNum;
+  while (i>=0) {
+    console.log(i);
+    i--
+  }
+}
+countdown(5);
